b456b890f9f71e15fc5dc83ff0861d8523f4b38a
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2019-11-26 09:21:34 -0300

    Continuing to learn about it. Message service is now working.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const email = document.getElementById("email"),
       password = document.getElementById("password"),
       buttonRegister = document.getElementById("buttonRegister"),
       buttonLogin = document.getElementById("buttonLogin"),
+      buttonSend = document.getElementById("buttonSend");
       answer = document.getElementById("answer");
+
+var db = firebase.firestore();
 
 buttonRegister.onclick = () => {
     firebase.auth().createUserWithEmailAndPassword(email.value,password.value)
@@ -15,6 +18,22 @@ buttonRegister.onclick = () => {
 
 buttonLogin.onclick = () => {
     firebase.auth().signInWithEmailAndPassword(email.value,password.value)
+        .then(() => {
+            div_login.style.display = "none";
+            div_msg.style.display = "block";
+
+            db.collection("messages").get().then(
+                (result) => {
+                    // console.log(result);
+                    result.forEach(
+                        (doc) => {
+                            p_message.innerHTML += `${doc.data().message}<br>`;
+                            console.log(doc.data().message);
+                        }
+                    )
+                }
+            )
+            })
         .catch(
             (error) => {
                 answer.innerText = `${error.code}: ${error.message}`;
@@ -25,7 +44,7 @@ buttonLogin.onclick = () => {
     if (user){
         user.updateProfile({
             displayName:"Christian",
-            phoneNumber:"+5521995465308",
+            // phoneNumber:"+5521999999999",
             photoURL:"photourl",
         })
             .then(() => {})
@@ -33,6 +52,38 @@ buttonLogin.onclick = () => {
                 (error) => {
                     answer.innerText = `${error.code}: ${error.message}`;
             });
-        answer.innerText = `${user.email}` + `${user.displayName}` + `${user.phoneNumber}` +`${user.photoURL}`;
+        // answer.innerText = `${user.email}` + `${user.displayName}` + `${user.phoneNumber}` +`${user.photoURL}`;
     }
+};
+
+// buttonSend.onclick = () => {
+//     db.collection("cities").doc("message").set(
+//         {
+//             country : "BR",
+//             created_at : new Date(Date.now()).toUTCString(),
+//             updated_at : new Date(Date.now()).toUTCString()
+//         }
+//     ).then(() => {
+//         console.log("City logged!")
+//     }).catch((error) => {
+//         console.log(`${error.code}: ${error.message}`);
+//     });
+// };
+
+db.collection("message").onSnapshot().then(() => {});
+
+buttonSend.onclick = () => {
+    let user = firebase.auth().currentUser;
+    db.collection("messages").add(
+        {
+            message : message.value,
+            user : user.uid,
+            created_at : new Date(Date.now()).toUTCString(),
+            update_at : new Date(Date.now()).toUTCString()
+        }
+    ).then(() => {
+        console.log("Message sent!")
+    }).catch((error) => {
+        console.log(`${error.code}: ${error.message}`);
+    })
 };
